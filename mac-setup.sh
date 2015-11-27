5471067cebb09d437770b32cdeb6c1ced8ca0a61
Von Welch <von@vwelch.com>
2015-11-26 19:35:50 -0500

    Added send-to-kindle

--- mac-setup.sh
+++ mac-setup.sh
@@ -320,6 +320,7 @@ cask_install evernote
 cask_install sketchup
 cask_install picasa
 cask_install kindle
+cask_install send-to-kindle
 cask_install spark  # http://www.shadowlab.org/Software/spark.php
 cask_install plain-clip  # http://www.bluem.net/en/mac/plain-clip/
 cask_install hiarcs-chess-explorer  # I have a license
